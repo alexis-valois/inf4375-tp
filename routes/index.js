98b2285f0f6bee239c3e6673b4d12dfe32bf23b6
Alexis Valois-Adamowicz <valois-adamowicz.alexis@courrier.uqam.ca>
2016-11-09 11:25:32 -0500

    WIP Q.A5

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'INF4375' });
+  res.render('index', { title: 'Rechercher des contrevenants par date' });
 });
 
 module.exports = router;
