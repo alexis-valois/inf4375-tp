caf0d25ef7741a0f9d9c42432cfb1e1631ff5ca5
Alexis Valois-Adamowicz <valois-adamowicz.alexis@courrier.uqam.ca>
2016-11-19 12:20:23 -0500

    commentaires contrevenants.js

--- routes/contrevenants.js
+++ routes/contrevenants.js
@@ -12,6 +12,7 @@ var contrevenantSchema = require('../validations/contrevenant');
 var collName = 'contrevenants';
 var error500message = "Une erreur est survenue côté serveur. Veuillez réessayer plustard.";
 
+/*Fonction qui retourne l'ensemble des contrevenants contenus dans la bd et renvoit le résultat dans la réponse http.*/
 var findAllContrevenants = function(res) {
 	mongoService.findAll(collName, function(err, result){
 	    if (err){
@@ -28,6 +29,7 @@ var findAllContrevenants = function(res) {
 	});
 }
 
+/*Retourne la list des contrevenants dont leur date d'infraction se situe dans une plage donnée dans l'url.*/
 var handleDateFiltering = function(req, res){
 	var from = moment(req.query.du);
 	var to = moment(req.query.au);
@@ -45,6 +47,7 @@ var handleDateFiltering = function(req, res){
 	});
 }
 
+/*Effectue la validation des dates reçues dans l'url.*/
 var validateDates = function(req, res, next){
 	if (!req.query.du){
 		var err = new Error("La date de début doit être présente.");
@@ -72,6 +75,7 @@ var validateDates = function(req, res, next){
 	
 }
 
+/*Détermine quel comportement effectuer en fonction de la présence ou non des paramètres de filtrage par date dans l'url.*/
 var dispatchFromParams = function(req, res, next){
 	if (Object.keys(req.query).length === 0){
 		findAllContrevenants(res);
@@ -82,6 +86,7 @@ var dispatchFromParams = function(req, res, next){
 
 router.get('/', [dispatchFromParams, validateDates, handleDateFiltering]);
 
+/*Valide la format de l'id fournit dans l'url.*/
 var validateContrevenantId = function(req, res, next){
 	/*source : https://github.com/jacquesberger/exemplesINF4375/blob/master/Ateliers/json-schema/Solutions/routes/index.js*/
 	var id = req.params.id;
@@ -96,7 +101,8 @@ var validateContrevenantId = function(req, res, next){
 	}
 }
 
-var validateContrevenantBody = function(req, res, next){
+/*Détermine si le corps d'une requête http est présent.*/
+var isBodyPresent = function(req, res, next){
 	/*Source : http://stackoverflow.com/questions/4994201/is-object-empty*/
 	if (! (Object.getOwnPropertyNames(req.body).length > 0)){
 		var emptyBody = new Error("Le corps de la requête ne doit pas être vide.");
@@ -107,6 +113,7 @@ var validateContrevenantBody = function(req, res, next){
 	}
 }
 
+/*Détermine si l'id (déjà validé) est présent ou nom dans la bd Mongo.*/
 var validateIdExists = function(req, res, next){
 	mongoService.findById(collName, req.params.id, function(err, result){
 		if (err){
@@ -122,6 +129,7 @@ var validateIdExists = function(req, res, next){
 	});
 }
 
+/*Effectue la validation par json-schema du coprs de la requêtes http.*/
 var validateContrevenantBody = function(req, res, next){
 	var validation = validate(req.body, contrevenantSchema.update);
 	if (validation.errors.length > 0) {
@@ -142,8 +150,9 @@ var updateContrevenant = function(req, res){
 	});
 }
 /*source : http://expressjs.com/fr/guide/routing.html*/
-router.put('/:id', [validateContrevenantId, validateContrevenantBody, validateIdExists, validateContrevenantBody, updateContrevenant]);
+router.put('/:id', [validateContrevenantId, validateIdExists, isBodyPresent, validateContrevenantBody, updateContrevenant]);
 
+/*Supprimer un contrevenant selon son id*/
 var deleteContrevenant = function(req, res){
 	mongoService.delete(collName, req.params.id, function(err, result){
 		if (err){
