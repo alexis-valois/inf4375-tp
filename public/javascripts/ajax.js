2b1dd2c842ad13ce23db303d60425679025f5184
Alexis Valois-Adamowicz <valois-adamowicz.alexis@courrier.uqam.ca>
2016-12-01 20:58:49 -0500

    correction gestion erreur frontend

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,4 +1,6 @@
+$("#errors").html('');
 var replaceContrevenantTable = function(data){
+	$("#errors").html('');
 	if (data.length > 0){
 		$("#etablissementsInfractions").html('');
 		for (var i = 0; i < data.length; i++){
@@ -16,6 +18,10 @@ var replaceContrevenantTable = function(data){
 	}
 }
 
+var afficherErreur = function(){
+	$("#errors").html('<p><h3><span>Une erreur est survenue en communiquant avec le serveur.</span></h3></p>');
+}
+
 $(function(){
 	$("#search").click(function(){
 		var dateDebut = $("#debut").val();
@@ -25,9 +31,7 @@ $(function(){
 			type: "GET",
 			data: {du: dateDebut, au: dateFin},
 			dataType: 'json',
-			error: function(jqXHR, textStatus, errorThrown){
-				alert(errorThrown);
-			},
+			error: afficherErreur,
 			success : replaceContrevenantTable
 		});
 	});
